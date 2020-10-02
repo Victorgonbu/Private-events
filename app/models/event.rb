3c2571e126b588b77101fcf1f1ec69f549f6986d
Shaqri <peter200970@hotmail.com>
2020-10-02 18:59:12 -0500

    add user show page and event show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :invitations
-  has_many :attendee, through: :invitations
+  has_many :attendees, through: :invitations
 end
