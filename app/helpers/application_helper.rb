b71cecfebbb4394d8cae94002419d98ad78f1f77
Shaqri <peter200970@hotmail.com>
2020-10-15 12:27:38 -0500

    add Readme file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def user_logged_in?
     session[:user_id].nil? ? false : true
   end
+
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
