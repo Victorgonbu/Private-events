27d6bf4116882d73c653616663c34bdb5518b11a
Shaqri <peter200970@hotmail.com>
2020-10-22 01:41:43 -0500

    Refactor user view logic into user_helper and partials

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def display_hosted_events
+    render 'hosted_events' if current_user.events.any?
+  end
+
+  def display_invited_events
+    render 'invited_events' if current_user.attended_events.any?
+  end
 end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,5 +1,4 @@
 class Invitation < ApplicationRecord
   belongs_to :attendee, class_name: 'User'
   belongs_to :event
-  validates :attendee_id, uniqueness: true
 end
